fbbe0a59c66f885d1627e3b8b604ec30fe053bbd
Julien Rey <rey.julien84@gmail.com>
2025-12-29 22:41:02 +0100

    ajout de la fonction tresorerie

--- fruit_manager.py
+++ fruit_manager.py
@@ -1,15 +1,30 @@
 import json
 
 
-def ouvrir_inventaire(path="inventaire.json"):
+def ouvrir_inventaire(path="data/inventaire.json"):
     with open(path, mode="r", encoding="utf-8") as file:
         inventaire = json.load(file)
     return inventaire
 
 
-def ecrire_inventaire(inventaire, path="inventaire.json"):
+def ecrire_inventaire(inventaire, path="data/inventaire.json"):
     with open(path, mode="w", encoding="utf-8") as file:
         json.dump(inventaire, file, indent=4, ensure_ascii=False)
+
+
+def ouvrir_tresorerie(path="data/tresorerie.txt"):
+    with open(path, mode="r", encoding="utf-8") as file:
+        tresorerie = int(file.readline())
+    return tresorerie
+
+
+def ecrire_tresorerie(tresorerie, path="data/tresorerie.txt"):
+    with open(path, mode="w", encoding="utf-8") as file:
+        file.write(str(tresorerie))
+
+
+def afficher_tresorerie(tresorerie):
+    print(f"💰 Trésrorie : {tresorerie}")
 
 
 def afficher_inventaire(inventaire):
@@ -23,16 +38,23 @@ def recolter(inventaire, fruit, quantite):
     print(f"✅ récolté {quantite} {fruit} supplémentaires ")
 
 
-def vendre(inventaire, fruit, quantite):
+def vendre(inventaire, fruit, quantite, tresorerie):
     inventaire[fruit] = inventaire.get(fruit, 0) - quantite
-    print(f"✅ vendu {quantite} {fruit} supplémentaires ")
+    tresorerie += 1 * quantite
+    print(f"💰 vendu {quantite} {fruit} supplémentaires ")
+    return (inventaire, tresorerie)
 
 
 if __name__ == "__main__":
 
     inventaire = ouvrir_inventaire()
+    tresorerie = ouvrir_tresorerie()
+    afficher_tresorerie(tresorerie)
     afficher_inventaire(inventaire)
     recolter(inventaire, "bananes", 10)
     afficher_inventaire(inventaire)
-    vendre(inventaire, "bananes", 5)
+    inventaire, tresorerie = vendre(inventaire, "bananes", 5, tresorerie)
+    ecrire_inventaire(inventaire)
+    ecrire_tresorerie(tresorerie)
     afficher_inventaire(inventaire)
+    afficher_tresorerie(tresorerie)
