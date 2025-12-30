78dd12846ddac86ed52a3c71643bcb86aa05c2b6
Julien Rey <rey.julien84@gmail.com>
2025-12-30 21:56:15 +0100

    ajout de la fonction de prix

--- fruit_manager.py
+++ fruit_manager.py
@@ -12,6 +12,12 @@ def ecrire_inventaire(inventaire, path="data/inventaire.json"):
         json.dump(inventaire, file, indent=4, ensure_ascii=False)
 
 
+def ouvrir_prix(path="data/prix.json"):
+    with open(path, mode="r", encoding="utf-8") as file:
+        prix = json.load(file)
+    return prix
+
+
 def ouvrir_tresorerie(path="data/tresorerie.txt"):
     with open(path, mode="r", encoding="utf-8") as file:
         tresorerie = int(file.readline())
@@ -38,9 +44,9 @@ def recolter(inventaire, fruit, quantite):
     print(f"✅ récolté {quantite} {fruit} supplémentaires ")
 
 
-def vendre(inventaire, fruit, quantite, tresorerie):
+def vendre(inventaire, fruit, quantite, tresorerie, prix):
     inventaire[fruit] = inventaire.get(fruit, 0) - quantite
-    tresorerie += 1 * quantite
+    tresorerie += prix.get(fruit, 0) * quantite
     print(f"💰 vendu {quantite} {fruit} supplémentaires ")
     return (inventaire, tresorerie)
 
@@ -49,11 +55,12 @@ if __name__ == "__main__":
 
     inventaire = ouvrir_inventaire()
     tresorerie = ouvrir_tresorerie()
+    prix = ouvrir_prix()
     afficher_tresorerie(tresorerie)
     afficher_inventaire(inventaire)
     recolter(inventaire, "bananes", 10)
     afficher_inventaire(inventaire)
-    inventaire, tresorerie = vendre(inventaire, "bananes", 5, tresorerie)
+    inventaire, tresorerie = vendre(inventaire, "bananes", 5, tresorerie, prix)
     ecrire_inventaire(inventaire)
     ecrire_tresorerie(tresorerie)
     afficher_inventaire(inventaire)
