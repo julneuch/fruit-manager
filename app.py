a3cd022d0228a3c208cf5070db84054abff0f913
Julien Rey <rey.julien84@gmail.com>
2026-01-02 09:34:25 +0100

    Ajout dune side bar avec interactivité

--- app.py
+++ app.py
@@ -12,3 +12,25 @@ st.metric(label="montant disponible", value=f"{tresorerie:.2f} $")
 
 st.header("Inventaire")
 st.table(inventaire)
+
+
+# ---sidebar---#
+with st.sidebar:
+    st.title("Actions")
+    action = st.selectbox(
+        "Choisir une action", options=["Récolter", "Vendre"], key="action_choisie"
+    )
+    fruit = st.selectbox(
+        "Choisir un fruit", options=inventaire.keys(), key="fruit_choisi"
+    )
+    quantite = st.number_input("Quantité", min_value=1, step=1, key="quantite_choisie")
+    if st.button("Valider"):
+        if action == "Récolter":
+            recolter(inventaire, fruit, quantite)
+        elif action == "Vendre":
+            inventaire, tresorerie = vendre(
+                inventaire, fruit, quantite, tresorerie, prix
+            )
+        ecrire_inventaire(inventaire)
+        ecrire_tresorerie(tresorerie)
+        st.rerun()
